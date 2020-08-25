957b5e3e2a12335d79434df3cbe44d9b1815f26b
Tenji <patricepetit@hotmail.com>
2020-08-25 07:49:42 +0200

    feature/antd

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-    <Counter />
-    <div>
-      <router-link to="/">Go to Home</router-link>
-    </div>
-    <div div>
-      <router-link to="/about">Go to About</router-link>
-    </div>
-    <div div>
-      <router-link to="/users">Go to Users</router-link>
-    </div>
-    <router-view></router-view>
-  </div>
+    <Layout />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-import Counter from "./components/Counter.vue";
+import Layout from "./views/Layout.vue";
 import "./assets/styles/app.less";
 
 export default defineComponent({
   components: {
-    HelloWorld,
-    Counter
+    Layout
   },
   data() {
     return {
